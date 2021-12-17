5932fcd6a686585015fa2799850a804e8428ffa3
Joel W. Reed <reedjw@ornl.gov>
2021-12-17 10:00:44 -0500

    Patched to try and fix this error during bulk load:
    [08:30:58Z] REDUCE 02h46m49s 19.23% edge_count:4.189G edge_speed:1.334M/sec plist_count:1.015G plist_speed:323.1k/sec. Num Encoding MBs: 0. jemalloc: 448 MiB
    panic: z.Buffer max size exceeded: 68719476736 offset: 68719476724 grow: 50
    
    goroutine 12167 [running]:
    github.com/dgraph-io/ristretto/z.(*Buffer).Grow(0xc02c4a4000, 0x32)
        /go/pkg/mod/github.com/dgraph-io/ristretto@v0.1.1-0.20210824115121-89e99415887a/z/buffer.go:180 +0x56e
    github.com/dgraph-io/ristretto/z.(*Buffer).SliceAllocate(0xc02c4a4000, 0x2e)
        /go/pkg/mod/github.com/dgraph-io/ristretto@v0.1.1-0.20210824115121-89e99415887a/z/buffer.go:266 +0x2e
    github.com/dgraph-io/dgraph/dgraph/cmd/bulk.(*mapIterator).Next(0xc027cfaf60, 0xc02c4a4000, {0xc00378fd00, 0x1f, 0x20})
        /ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/bulk/reduce.go:206 +0x127
    github.com/dgraph-io/dgraph/dgraph/cmd/bulk.(*reducer).reduce.func2()
        /ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/bulk/reduce.go:488 +0x5cd
    created by github.com/dgraph-io/dgraph/dgraph/cmd/bulk.(*reducer).reduce
        /ext-go/1/src/github.com/dgraph-io/dgraph/dgraph/cmd/bulk/reduce.go:478 +0x465

--- dgraph/cmd/bulk/reduce.go
+++ dgraph/cmd/bulk/reduce.go
@@ -438,7 +438,9 @@ func bufferStats(cbuf *z.Buffer) {
 func getBuf(dir string) *z.Buffer {
 	return z.NewBuffer(64<<20, "Reducer.GetBuf").
 		WithAutoMmap(1<<30, filepath.Join(dir, bufferDir)).
-		WithMaxSize(64 << 30)
+		// WithMaxSize(64 << 30)
+		// JWR change
+		WithMaxSize(0)
 }
 
 func (r *reducer) reduce(partitionKeys [][]byte, mapItrs []*mapIterator, ci *countIndexer) {
