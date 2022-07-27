e2e4296ec029d4fcbe2bec0e543c05a83f682d8a
Joel W. Reed <reedjw@ornl.gov>
2022-07-27 15:37:37 -0400

    Another attempt with buildx multi-arch.

--- docker-build-multi-arch-image/build.sh
+++ docker-build-multi-arch-image/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BUILDER=dgraph-builder-amd64-arm64
-IMAGE=code-int.ornl.gov:4567/torro/dgraph:v21.03.2-patch004-ma-rc2
+IMAGE=code-int.ornl.gov:4567/torro/dgraph:v21.03.2-patch004-ma-2
 
 docker buildx create --name $BUILDER --platform linux/amd64,linux/arm64
 docker buildx build --builder $BUILDER -t $IMAGE --push --progress plain --platform linux/amd64,linux/arm64 .
